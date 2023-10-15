d536341426b7889a3cacf67ab442061236ce55ce
forishka <fomina.ii@mail.ru>
2023-10-15 22:36:05 +0300

    Added 7 hw

--- HW07-pivot/hw_cross_apply_pivot_tasks-188-2362fd.sql
+++ HW07-pivot/hw_cross_apply_pivot_tasks-188-2362fd.sql
@@ -39,7 +39,24 @@ InvoiceMonth | Peeples Valley, AZ | Medicine Lodge, KS | Gasport, NY | Sylvanite
 -------------+--------------------+--------------------+-------------+--------------+------------
 */
 
-напишите здесь свое решение
+;WITH PivotData as (
+	SELECT 
+		CA.[Date],
+		CA2.CustomerName,
+		[OrderID]
+	FROM [Sales].[Invoices] as i
+	JOIN Sales.Customers as c on c.CustomerID = i.CustomerID
+	CROSS APPLY (SELECT FORMAT(DATEADD(mm, DATEDIFF(mm, 0, [InvoiceDate]), 0), 'dd.MM.yyyy') as [Date]) as CA
+	CROSS APPLY (SELECT Substring(CustomerName, Charindex('(', CustomerName) + 1, Charindex(')', CustomerName) - Charindex('(', CustomerName) - 1) as [CustomerName]) as CA2
+	WHERE i.[CustomerID] BETWEEN 2 and 6
+)
+
+SELECT *
+FROM PivotData
+PIVOT ( COUNT([OrderID]) FOR CustomerName
+		IN ([Gasport, NY], [Jessie, ND], [Medicine Lodge, KS], [Peeples Valley, AZ], [Sylvanite, MT])
+	) as PivotTable
+ORDER BY CAST([Date] as date)
 
 /*
 2. Для всех клиентов с именем, в котором есть "Tailspin Toys"
@@ -56,7 +73,19 @@ Tailspin Toys (Head Office) | Ribeiroville
 ----------------------------+--------------------
 */
 
-напишите здесь свое решение
+SELECT [CustomerName], [AddressLine]
+FROM (
+	SELECT
+		[CustomerName],
+		[DeliveryAddressLine1],
+		[DeliveryAddressLine2],
+		[PostalAddressLine1],
+		[PostalAddressLine2]
+	FROM [Sales].[Customers]
+	WHERE [CustomerName] LIKE 'Tailspin Toys%'
+	) as T
+UNPIVOT (AddressLine for AddressLines 
+		IN ([DeliveryAddressLine1], [DeliveryAddressLine2], [PostalAddressLine1], [PostalAddressLine2])) as unpt
 
 /*
 3. В таблице стран (Application.Countries) есть поля с цифровым кодом страны и с буквенным.
@@ -74,11 +103,57 @@ CountryId | CountryName | Code
 ----------+-------------+-------
 */
 
-напишите здесь свое решение
+
+SELECT [CountryID], [CountryName], [Code]
+FROM (
+	SELECT
+		[CountryID]
+		,[CountryName]
+		,[IsoAlpha3Code]
+		,CAST([IsoNumericCode] as nvarchar(3)) as [IsoNumericCode]
+	FROM [WideWorldImporters].[Application].[Countries]
+	) AS PivotTable
+UNPIVOT (Code for PivotColumns
+		IN ([IsoAlpha3Code], [IsoNumericCode])) as upvt
 
 /*
 4. Выберите по каждому клиенту два самых дорогих товара, которые он покупал.
 В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки.
 */
 
-напишите здесь свое решение
+-- с использованием CROSS APPLY
+SELECT A.[CustomerID], A.CustomerName, ca.*, InvoiceDate
+FROM Sales.Customers as A
+CROSS APPLY (
+	SELECT TOP 2 [StockItemID], MAX(UnitPrice) as UnitPrice
+	FROM Sales.InvoiceLines as l
+	JOIN [Sales].[Invoices] as i on l.InvoiceID = i.InvoiceID
+	WHERE i.[CustomerID] = A.[CustomerID]
+	GROUP BY [StockItemID]
+	ORDER BY MAX(UnitPrice) DESC
+	) AS ca
+CROSS APPLY (
+	SELECT InvoiceDate
+	FROM [Sales].[Invoices] as ii
+	JOIN Sales.InvoiceLines as ll on ll.InvoiceID = ii.InvoiceID
+	WHERE ii.CustomerID = A.[CustomerID] and ll.StockItemID = ca.StockItemID
+	) as ca2
+ORDER BY [CustomerID], UnitPrice DESC, [StockItemID], InvoiceDate
+
+-- с использованием оконной функции
+SELECT [CustomerID], CustomerName, [StockItemID], UnitPrice, InvoiceDate
+FROM (
+	SELECT 
+		i.[CustomerID],
+		c.CustomerName,
+		l.[StockItemID],
+		l.Description,
+		l.UnitPrice,
+		DENSE_RANK() OVER (PARTITION BY i.[CustomerID] ORDER BY UnitPrice DESC) as RN,
+		i.InvoiceDate
+	FROM [Sales].[Invoices] as i
+	JOIN Sales.InvoiceLines as l on l.InvoiceID = i.InvoiceID
+	JOIN Sales.Customers as c on c.CustomerID = i.CustomerID
+) as T
+WHERE rn in (1, 2)
+ORDER BY [CustomerID], UnitPrice DESC, [StockItemID], InvoiceDate
