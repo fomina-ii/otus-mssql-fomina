2bad2d825092c8bd4b2d49ca54e506e95643a7c1
forishka <fomina.ii@mail.ru>
2023-10-23 20:39:05 +0300

    added 8 hw

---
+++ HW08-dynamic/Scripts/1 variables.sql
@@ -0,0 +1,37 @@
+USE [WideWorldImporters]
+
+---- объ€вление переменных
+--вариант 1
+DECLARE @CustomerId INT,
+		@CustId INT = 0,
+		@InvoiceDate DATE;
+
+--- инициализаци€
+set @CustId = 10;
+set @CustomerId = (select max(CustomerID) from Sales.Customers);
+select @CustId, @CustomerId;
+
+
+--вариант 2
+Declare @InvoiceId INT;
+DECLARE @TransactionDate DATE;
+DECLARE @CustomerName NVARCHAR(100);
+
+SELECT 
+	@InvoiceId = InvoiceID,
+	@TransactionDate = TransactionDate
+FROM Sales.CustomerTransactions
+ORDER BY TransactionDate DESC;
+
+select @InvoiceId, @TransactionDate;
+
+--ќдинаковое ли будет значение?
+SELECT TOP 1
+	@InvoiceId = InvoiceID,
+	@TransactionDate = TransactionDate
+FROM Sales.CustomerTransactions
+ORDER BY TransactionDate DESC;
+
+select @InvoiceId, @TransactionDate;
+
+
---
+++ HW08-dynamic/hw_dynamic_sql.sql
@@ -0,0 +1,68 @@
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+
+Это задание из занятия "Операторы CROSS APPLY, PIVOT, UNPIVOT."
+Нужно для него написать динамический PIVOT, отображающий результаты по всем клиентам.
+Имя клиента указывать полностью из поля CustomerName.
+
+Требуется написать запрос, который в результате своего выполнения 
+формирует сводку по количеству покупок в разрезе клиентов и месяцев.
+В строках должны быть месяцы (дата начала месяца), в столбцах - клиенты.
+
+Дата должна иметь формат dd.mm.yyyy, например, 25.12.2019.
+
+Пример, как должны выглядеть результаты:
+-------------+--------------------+--------------------+----------------+----------------------
+InvoiceMonth | Aakriti Byrraju    | Abel Spirlea       | Abel Tatarescu | ... (другие клиенты)
+-------------+--------------------+--------------------+----------------+----------------------
+01.01.2013   |      3             |        1           |      4         | ...
+01.02.2013   |      7             |        3           |      4         | ...
+-------------+--------------------+--------------------+----------------+----------------------
+*/
+
+
+DECLARE @dml AS NVARCHAR(MAX)
+DECLARE @ColumnName AS NVARCHAR(MAX)
+
+SELECT @ColumnName = ISNULL(@ColumnName + ',','') + QUOTENAME(CustomerName)
+FROM (SELECT DISTINCT [CustomerName]
+	  FROM [Sales].[Invoices] as i
+	  JOIN Sales.Customers as c on c.CustomerID = i.CustomerID
+	 ) AS names
+
+SELECT @ColumnName as ColumnName 
+
+SET @dml = 
+  N'SELECT [Date], ' +@ColumnName + ' FROM
+  (
+  SELECT 
+		CA.[Date],
+		CustomerName,
+		[OrderID]
+	FROM [Sales].[Invoices] as i
+	JOIN Sales.Customers as c on c.CustomerID = i.CustomerID
+	CROSS APPLY (SELECT FORMAT(DATEADD(mm, DATEDIFF(mm, 0, [InvoiceDate]), 0), ''dd.MM.yyyy'') as [Date]) as CA
+	WHERE i.[CustomerID] BETWEEN 2 and 6
+	) as PivotTable
+    PIVOT ( COUNT([OrderID]) FOR CustomerName
+		IN (' + @ColumnName + ')) AS PVTTable
+	ORDER BY CAST([Date] as date)'
+
+
+EXEC sp_executesql @dml
